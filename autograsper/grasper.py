47278056922e473a7c3a30f641d6f8127b6a7fd2
Axel Kaliff <axel.kaliff@protonmail.com>
2025-03-03 15:39:09 +0100

    cleanup

--- autograsper/grasper.py
+++ autograsper/grasper.py
@@ -5,7 +5,6 @@ import sys
 import time
 from enum import Enum
 from typing import List, Tuple
-import numpy as np
 import threading
 from dotenv import load_dotenv
 
@@ -16,7 +15,6 @@ if project_root not in sys.path:
 
 from client.cloudgripper_client import GripperRobot
 import library.utils as utils
-from library.utils import OrderType
 
 load_dotenv()
 
@@ -98,6 +96,7 @@ class AutograsperBase(ABC):
         """Wait for the start event, checking periodically for shutdown."""
         while not self.shutdown_event.is_set():
             if self.start_event.wait(timeout=0.05):
+                self.start_event.clear()  # Clear for next cycle.
                 return
 
     def run_grasping(self):
@@ -113,7 +112,6 @@ class AutograsperBase(ABC):
                 self.state = RobotActivity.ACTIVE
             elif self.state == RobotActivity.ACTIVE:
                 self.wait_for_start_signal()
-                self.start_event.clear()  # Clear for next cycle.
                 try:
                     self.perform_task()
                 except Exception as e:
@@ -191,140 +189,140 @@ class AutograsperBase(ABC):
             ):
                 self.record_current_state()
 
-    # CG1Specific
-    def manual_control(self, step_size=0.1, state=None, time_between_orders=None):
-        """
-        Manually control the robot using keyboard inputs.
-        """
-        from pynput import keyboard
+    # # CG1Specific
+    # def manual_control(self, step_size=0.1, state=None, time_between_orders=None):
+    #     """
+    #     Manually control the robot using keyboard inputs.
+    #     """
+    #     from pynput import keyboard
 
-        if self.robot_state is None:
-            self.robot_state, _ = self.get_state()
-        if time_between_orders is None:
-            time_between_orders = self.time_between_orders
+    #     if self.robot_state is None:
+    #         self.robot_state, _ = self.get_state()
+    #     if time_between_orders is None:
+    #         time_between_orders = self.time_between_orders
 
-        current_x = self.robot_state["x_norm"]
-        current_y = self.robot_state["y_norm"]
-        current_z = self.robot_state["z_norm"]
-        current_rotation = self.robot_state["rotation"]
-        current_angle = self.robot_state["claw_norm"]
+    #     current_x = self.robot_state["x_norm"]
+    #     current_y = self.robot_state["y_norm"]
+    #     current_z = self.robot_state["z_norm"]
+    #     current_rotation = self.robot_state["rotation"]
+    #     current_angle = self.robot_state["claw_norm"]
 
-        def on_press(key):
-            nonlocal current_x, current_y, current_z, current_rotation, current_angle
-            try:
-                # == XY axis ==
-                if key.char == "w":
-                    current_y += step_size
-                    current_y = min(max(current_y, 0), 1)
-                    self.queue_orders(
-                        [(OrderType.MOVE_XY, [current_x, current_y])],
-                        time_between_orders,
-                    )
-                elif key.char == "a":
-                    current_x -= step_size
-                    current_x = min(max(current_x, 0), 1)
-                    self.queue_orders(
-                        [(OrderType.MOVE_XY, [current_x, current_y])],
-                        time_between_orders,
-                    )
-                elif key.char == "s":
-                    current_y -= step_size
-                    current_y = min(max(current_y, 0), 1)
-                    self.queue_orders(
-                        [(OrderType.MOVE_XY, [current_x, current_y])],
-                        time_between_orders,
-                    )
-                elif key.char == "d":
-                    current_x += step_size
-                    current_x = min(max(current_x, 0), 1)
-                    self.queue_orders(
-                        [(OrderType.MOVE_XY, [current_x, current_y])],
-                        time_between_orders,
-                    )
+    #     def on_press(key):
+    #         nonlocal current_x, current_y, current_z, current_rotation, current_angle
+    #         try:
+    #             # == XY axis ==
+    #             if key.char == "w":
+    #                 current_y += step_size
+    #                 current_y = min(max(current_y, 0), 1)
+    #                 self.queue_orders(
+    #                     [(OrderType.MOVE_XY, [current_x, current_y])],
+    #                     time_between_orders,
+    #                 )
+    #             elif key.char == "a":
+    #                 current_x -= step_size
+    #                 current_x = min(max(current_x, 0), 1)
+    #                 self.queue_orders(
+    #                     [(OrderType.MOVE_XY, [current_x, current_y])],
+    #                     time_between_orders,
+    #                 )
+    #             elif key.char == "s":
+    #                 current_y -= step_size
+    #                 current_y = min(max(current_y, 0), 1)
+    #                 self.queue_orders(
+    #                     [(OrderType.MOVE_XY, [current_x, current_y])],
+    #                     time_between_orders,
+    #                 )
+    #             elif key.char == "d":
+    #                 current_x += step_size
+    #                 current_x = min(max(current_x, 0), 1)
+    #                 self.queue_orders(
+    #                     [(OrderType.MOVE_XY, [current_x, current_y])],
+    #                     time_between_orders,
+    #                 )
 
-                # == Z axis ==
-                elif key.char == "r":
-                    current_z += step_size
-                    current_z = min(max(current_z, 0), 1)
-                    print(current_z)
-                    self.queue_orders(
-                        [(OrderType.MOVE_Z, [current_z])], time_between_orders
-                    )
-                elif key.char == "f":
-                    current_z -= step_size
-                    current_z = min(max(current_z, 0), 1)
-                    print(current_z)
-                    self.queue_orders(
-                        [(OrderType.MOVE_Z, [current_z])], time_between_orders
-                    )
+    #             # == Z axis ==
+    #             elif key.char == "r":
+    #                 current_z += step_size
+    #                 current_z = min(max(current_z, 0), 1)
+    #                 print(current_z)
+    #                 self.queue_orders(
+    #                     [(OrderType.MOVE_Z, [current_z])], time_between_orders
+    #                 )
+    #             elif key.char == "f":
+    #                 current_z -= step_size
+    #                 current_z = min(max(current_z, 0), 1)
+    #                 print(current_z)
+    #                 self.queue_orders(
+    #                     [(OrderType.MOVE_Z, [current_z])], time_between_orders
+    #                 )
 
-                # == Gripper open ==
-                elif key.char == "i":
-                    current_angle += step_size / 100
-                    current_angle = min(current_angle, 1)
-                    print(current_angle)
-                    self.queue_orders(
-                        [(OrderType.GRIPPER_CLOSE, [current_angle])],
-                        time_between_orders,
-                    )
-                # == Gripper open small steps==
-                elif key.char == "o":
-                    current_angle += step_size / 200
-                    current_angle = min(current_angle, 1)
-                    print(current_angle)
-                    self.queue_orders(
-                        [(OrderType.GRIPPER_CLOSE, [current_angle])],
-                        time_between_orders,
-                    )
-                # == Gripper close ==
-                elif key.char == "k":
-                    current_angle -= step_size / 100
-                    current_angle = max(current_angle, 0.2)
-                    print(current_angle)
-                    self.queue_orders(
-                        [(OrderType.GRIPPER_CLOSE, [current_angle])],
-                        time_between_orders,
-                    )
-                # == Gripper close small steps==
-                elif key.char == "l":
-                    current_angle -= step_size / 200
-                    current_angle = max(current_angle, 0.2)
-                    print(current_angle)
-                    self.queue_orders(
-                        [(OrderType.GRIPPER_CLOSE, [current_angle])],
-                        time_between_orders,
-                    )
+    #             # == Gripper open ==
+    #             elif key.char == "i":
+    #                 current_angle += step_size / 100
+    #                 current_angle = min(current_angle, 1)
+    #                 print(current_angle)
+    #                 self.queue_orders(
+    #                     [(OrderType.GRIPPER_CLOSE, [current_angle])],
+    #                     time_between_orders,
+    #                 )
+    #             # == Gripper open small steps==
+    #             elif key.char == "o":
+    #                 current_angle += step_size / 200
+    #                 current_angle = min(current_angle, 1)
+    #                 print(current_angle)
+    #                 self.queue_orders(
+    #                     [(OrderType.GRIPPER_CLOSE, [current_angle])],
+    #                     time_between_orders,
+    #                 )
+    #             # == Gripper close ==
+    #             elif key.char == "k":
+    #                 current_angle -= step_size / 100
+    #                 current_angle = max(current_angle, 0.2)
+    #                 print(current_angle)
+    #                 self.queue_orders(
+    #                     [(OrderType.GRIPPER_CLOSE, [current_angle])],
+    #                     time_between_orders,
+    #                 )
+    #             # == Gripper close small steps==
+    #             elif key.char == "l":
+    #                 current_angle -= step_size / 200
+    #                 current_angle = max(current_angle, 0.2)
+    #                 print(current_angle)
+    #                 self.queue_orders(
+    #                     [(OrderType.GRIPPER_CLOSE, [current_angle])],
+    #                     time_between_orders,
+    #                 )
 
-                # == Rotate ==
-                elif key.char == "z":
-                    current_rotation -= int(step_size * 100)
-                    current_rotation = np.clip(current_rotation, 0, 360)
-                    print(current_rotation)
-                    self.queue_orders(
-                        [(OrderType.ROTATE, [current_rotation])], time_between_orders
-                    )
-                elif key.char == "x":
-                    print(current_rotation)
-                    current_rotation += int(step_size * 100)
-                    current_rotation = np.clip(current_rotation, 0, 360)
-                    self.queue_orders(
-                        [(OrderType.ROTATE, [current_rotation])], time_between_orders
-                    )
+    #             # == Rotate ==
+    #             elif key.char == "z":
+    #                 current_rotation -= int(step_size * 100)
+    #                 current_rotation = np.clip(current_rotation, 0, 360)
+    #                 print(current_rotation)
+    #                 self.queue_orders(
+    #                     [(OrderType.ROTATE, [current_rotation])], time_between_orders
+    #                 )
+    #             elif key.char == "x":
+    #                 print(current_rotation)
+    #                 current_rotation += int(step_size * 100)
+    #                 current_rotation = np.clip(current_rotation, 0, 360)
+    #                 self.queue_orders(
+    #                     [(OrderType.ROTATE, [current_rotation])], time_between_orders
+    #                 )
 
-                # == Quit ==
-                elif key.char == "q":
-                    return False
+    #             # == Quit ==
+    #             elif key.char == "q":
+    #                 return False
 
-            except Exception as e:
-                print(e)
-                print(
-                    "Make sure that the runtime has access to an X server. If running in a container on Wayland, you might need to perform `xhost local:root` in the host terminal."
-                )
+    #         except Exception as e:
+    #             print(e)
+    #             print(
+    #                 "Make sure that the runtime has access to an X server. If running in a container on Wayland, you might need to perform `xhost local:root` in the host terminal."
+    #             )
 
-        def on_release(key):
-            if key == keyboard.Key.esc:
-                # Stop listener
-                return False
+    #     def on_release(key):
+    #         if key == keyboard.Key.esc:
+    #             # Stop listener
+    #             return False
 
-        with keyboard.Listener(on_press=on_press, on_release=on_release) as listener:
-            listener.join()
+    #     with keyboard.Listener(on_press=on_press, on_release=on_release) as listener:
+    #         listener.join()
